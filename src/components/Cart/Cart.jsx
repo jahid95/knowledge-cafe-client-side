7588ac26ad7353bb461b207e60f75313354d47ac
Anwar <anwarjahid449@gmail.com>
2023-03-30 23:07:38 +0600

    create side component

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -3,12 +3,29 @@ import './Cart.css';
 
 const Cart = ({book}) => {
     console.log(book);
-    const {bookName,poster} = book;
+    const {bookName,poster, writer, writerName, published, readTime} = book;
 
     return (
         <div className='cart-container'>
             <img src={poster} alt="" />
+            
+            <div className='writer-details'>
+                <div className='writer-info'>
+                    <img src={writer} alt="" />
+                    <div className='writer-name'>
+                        <h3>{writerName}</h3>
+                        <p>{published}</p>
+
+                    </div>
+
+                </div>
+                <div className='bookmark-info'>
+                    <h4>{readTime} min read ...</h4>
+
+                </div>
+            </div>
             <h2>{bookName}</h2>
+            <a href="">Mark as read...</a>
         </div>
     );
 };
--- src/components/Side/Side.jsx
+++ src/components/Side/Side.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import './Side.css'
 
 const Side = () => {
     return (
-        <div>
-            <h2>Bookmarked</h2>
+        <div className='bookmark-container'>
+            <h4 className='spent-time'>Spent time on read: {} min</h4>
+            <div className='bookmark-blogs'>
+                <h2>Bookmarked Blogs:</h2>
+            </div>
         </div>
     );
 };
